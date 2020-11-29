8a1e2230f4b5e90f8540eb67e6d30faa4e05c98b
Ouassimmeftah <59920693+Ouassimmeftah@users.noreply.github.com>
2020-11-29 19:29:38 +0100

    Add files via upload

--- tp9.c
+++ tp9.c
@@ -53,39 +53,59 @@ int copie(const char *fichier1,const char *fichier2){
 }
 
 int copieRepertoire(const char *dossier1,const char *dossier2){
+    // On prépare l'ouverture du dossier source
     DIR * d1 = opendir(dossier1);
-
+    // levée d'erreur si avérée
     if(d1 == NULL){
         perror(dossier1); 
     }
+    // gestion des permissions du dossier source 
+    struct stat stat_buffer_folder_source;
+    int status_folder_source = stat(dossier1, &stat_buffer_folder_source);
+    int permission_source_folder = stat_buffer_folder_source.st_mode;
 
+    // on Vérifie bien que le dossier e destination existe, sinon, on le crée
+    // On lui donne les mêmes permissions que le dossier source
+    int target_folder_creation = mkdir(dossier2, permission_source_folder);
+
+    // le résultat stocke un code d'erreur, utile pour savoir si le dossier existait déjà
+    printf("code erreur de création du dossier cible %d", target_folder_creation);
+    
+    // On prépare une structure servant de buffer à l'analyse du dossier source
     struct dirent * structd ;
+
     // boucle qui liste le contenu du dossier
     while((structd = readdir(d1)) != NULL){
-        
-        char *source_filename = structd->d_name; // récupération du nom du fichier source courant
 
-            if (strcmp(source_filename, ".") ==0 && strcmp(source_filename,"..") ==0 ) {
+        // récupération du nom du fichier source courant 
+        char *source_filename = structd->d_name; 
+    
+            if (!(strcmp(source_filename, ".") ==0 && strcmp(source_filename,"..") ==0 )) {
 
                     printf("%s\n",source_filename);  
+
                     // allocation de la mémoire pour le chemin source
                     char *source_path = malloc(strlen(dossier1) + 1 + strlen(source_filename)+1);
                     strcpy(source_path,dossier1);
                     strcat(source_path,"/");
                     strcat(source_path,source_filename);
+
                     // allocation de la mémoire pour le chemin destinataire
                     char *target_path = malloc(strlen(dossier2) +1 + strlen(source_filename) +1);
-                    strcpy(target_path,source_filename);
+                    strcpy(target_path,dossier2);
                     strcat(target_path,"/");
                     strcat(target_path,source_filename);
-
+                    
+                    // Maintenant que l'on a le chemin du fichier source et que l'on a définit un chemin pour le fichier cible
+                    // On peut faire la copie
                     copie(source_path,target_path);
 
+                    // finalement on libère l'espace demandé pour les source path et target path pour éviter les fuites de mémoire
                     free(source_path);
                     free(target_path);
 
             }
-            else perror("fichier parent");  // debuggage
+            else perror("fichier parent");  // debuggage condition
                 
     }
 
