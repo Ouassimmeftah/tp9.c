c00d72c65545c94c17c194206ee909674a440122
Ouassimmeftah <59920693+Ouassimmeftah@users.noreply.github.com>
2020-12-04 17:34:33 +0100

    Update copierecursivetest.c

--- copierecursivetest.c
+++ copierecursivetest.c
@@ -9,6 +9,52 @@
 #include <dirent.h>
 
 
+int copie(const char *fichier1,const char *fichier2){
+ 
+    int f1 = open(fichier1, O_RDONLY);
+    int f2 = open(fichier2, O_WRONLY | O_CREAT | O_EXCL, 0666);
+
+    if(f1 == NULL)
+        perror(fichier1);    
+
+    struct stat stat_buffer;
+    int  status;
+
+    status = stat(fichier1, &stat_buffer);
+
+    int permission = stat_buffer.st_mode;
+
+    while (1){
+        // init buffer
+        char buffer[4096];
+        // on lit un bout ( readcount ) du fichier qu'on stocke dans le buffer
+        int rcnt = read(f1,buffer,sizeof(buffer));
+
+        if(rcnt == 0 ) {
+            printf("test c: %s\n",fichier1);
+            break;
+        }
+
+        //  partie ecriture
+        int pos = 0;
+        while (rcnt != 0)
+        {
+            // buffer + pos : buffer correspond a un pointeur et pour avoir
+            // l'endroit actuel ou on en est du buffer
+            int wcnt = write(f2,buffer+pos,rcnt);
+            rcnt -= wcnt;
+            pos += wcnt; 
+        }
+        
+
+    }
+
+    fchmod(f2,permission);
+    close(f1);
+    close(f2);
+    return 0;
+}
+
 int copieRecursive( const char * fichierOuDossierOnSaitPas, const char * dest){
 
     // On prépare l'ouverture du dossier source
